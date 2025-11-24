d994d3ccb0fc8e6adc232f31a85a9cb1d0730a31
jia0623 <ss653511@gmail.com>
2025-11-24 21:28:29 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,21 +1,25 @@
-// p1: Calculator (Function Practice)
 #include <iostream>
 using namespace std;
-
-// TODO: 實作 calculate 函數
-// 參數: opcode (1-4), num1, num2
-// 回傳: 根據 opcode 計算的結果
-int calculate(int opcode, int num1, int num2) {
-    // 在此實作你的程式碼
-    return 0;
+int calculate(int opcode,int num1, int num2){
+  if (opcode==1){
+    return num1+num2;
+  }
+  else if(opcode==2){
+    return num1-num2;
+  }
+  else if(opcode==3){
+    return num1*num2;
+    }
+  else{
+    return num1/num2;
+  }
 }
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    // TODO: 讀取輸入並呼叫 calculate 函數
-    // 提示: 使用 while(cin >> opcode >> num1 >> num2) 來讀取到 EOF
-    
+int main()
+{
+  int a,b,c;
+  while (cin>>a>>b>>c){
+  cout<<calculate(a,b,c)<<endl;
+  }
+  
     return 0;
 }
